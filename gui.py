b8eef24212d45c31e9ee741389985c57fe0137df
Christian Saunders <noname45688@gmail.com>
2020-04-15 18:22:48 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
+from functools import partial
 
 root = tk.Tk()
 apps=[]
@@ -27,6 +28,31 @@ def runApps():
     for app in apps:
         os.startfile(app)
 
+def remove(lb):
+    global apps
+    selection = lb.curselection()
+    for item in selection:
+        del apps[item] 
+        lb.delete(item)
+
+def removeApps():
+    global apps
+    top = tk.Toplevel()
+    canvas = tk.Canvas(top, height=500, width=500, bg="#263D42")
+    canvas.pack()
+    frame = tk.Frame(top, bg="white")
+    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+    lb = tk.Listbox(frame, height=300, width=300)
+    lb.pack()
+    counter=0
+    for app in apps:
+        lb.insert(counter, app)
+        counter+=1
+    action_with_arg = partial(remove, lb)
+    tk.Button(top, text="Remove", padx=10, pady=6, fg="white", bg="#263D42", command=action_with_arg).pack()
+    
+    
+
 canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
 canvas.pack()
 
@@ -38,6 +64,9 @@ openFile.pack()
 
 runApps = tk.Button(root, text="Run Apps", padx=10, pady=6, fg="white", bg="#263D42", command=runApps)
 runApps.pack()
+
+removeApps = tk.Button(root, text="Remove Apps", padx=10, pady=6, fg="white", bg="#263D42", command=removeApps)
+removeApps.pack()
 
 for app in apps:
     label=tk.Label(frame, text=app)
